9152ff707be5d43e4a17b23f984900ef5edf00a4
Gulrez Alam <egulrezalam@gmail.com>
2023-05-08 18:06:10 +0530

    CV link updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         {/* <a href={CV} download className='btn'>Downloads CV</a> */}
-        <a href='https://drive.google.com/file/d/1MAaPG9C6BS-iiAxFi8sfdtzw87jSiXAF/view?usp=sharing' className='btn' target='_blank'>Download CV</a>
+        <a href='https://drive.google.com/file/d/1GXt-DpVb_0bqiZwE0PwfTHi6WhlBiZu6/view?usp=share_link' className='btn' target='_blank'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         
     </div>
